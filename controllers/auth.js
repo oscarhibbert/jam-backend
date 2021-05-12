1ba1b6baa7bc801924a7ea0fc727ef23d7467466
Oscar <oscarhibbert1@gmail.com>
2021-05-12 21:49:35 +0100

    Created new service called AuthService with method loginUser. This method uses code from the old controller method and needed some refactoring. Refactored the auth controller method. Tested & working successfully.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,8 @@
 // This controller contains all authentication methods
 
-// Imports
-const config = require('config');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-// Model imports
-const User = require('../models/User');
+// Service imports
+const AuthService = require('../services/AuthService');
+const AuthServiceInstance = new AuthService();
 
 // Controller methods
 // @desc   Authenticate user & get token (login)
@@ -14,60 +10,25 @@ const User = require('../models/User');
 // @access Public
 exports.loginUser = async (req, res, next) => {
     try {
-        // Destructure req body
+        // Destructure req.body
         const { email, password } = req.body;
 
-        // Look for the user via email
-        let user = await User.findOne({ email });
+        let response = await AuthServiceInstance.loginUser(email, password);
 
-        // Check if the user exists
-        if (user) {
-
-            // Compare plain text & encrypted password
-            // user.password comes from User.findOne
-            const isMatch = await bcryptjs.compare(password, user.password);
-            
-            // If the passwords do not match
-            if (!isMatch) {
-                throw new Error('Invalid Credentials');
-            }
-
-            /* user.fineOne() returns a promise allowing us to access the
-            record id using user.id. Mongoose abstracts ._id to .id */
-
-            // Construct JWT payload
-            const payload = {
-                user: {
-                    id: user.id
-                },
-            };
-
-            // Sign & respond with the JWT token
-            jwt.sign(
-                payload,
-                config.get('jwtSecret'),
-                { expiresIn: config.get('jwtDuration') },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({
-                        success: true,
-                        msg: 'JWT token',
-                        token: token
-                    });
-                },
-            );
+        if (response.msg === 'Invalid Credentials') {
+            // Return a 401 authorisation denied
+            return res
+                .status(400)
+                .json({ success: false, msg: 'Invalid Credentials' });
         }
 
-        // Else...
-        else {
-            throw new Error('Invalid Credentials');
-        }
+        console.log(response);
+
+        res.json(response);
 
     } catch (err) {
         console.error(err.message);
-        if (err.message === 'Invalid Credentials') {
-            return res.status(400).json({ success: false, msg: 'Invalid Credentials' });
-        }
+
         res.status(500).json({ success: false, msg: 'Server Error' });
-    }
-}
+    };
+};
---
+++ services/AuthService.js
@@ -0,0 +1,113 @@
+// Imports
+const config = require('config');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Model imports
+const User = require('../models/User');
+
+// Events
+const EventEmitter = require('events').EventEmitter;
+const authServiceEvents = new EventEmitter;
+
+/**
+ * @description Create an instance of the AuthService class.
+ */
+module.exports = class AuthService {
+    /**
+     * @desc                         Login a user method.
+     * @param {string} userEmail     String containing user email.
+     * @param {string} userPassword  String containing user password.
+     * @return                       Object containing response including jwtToken if successful.
+     */
+    async loginUser(userEmail, userPassword) {
+        try {
+            let response;
+            let success;
+            let msg;
+            let jwtToken;
+        
+            // Look for the user by email address
+            let user = await User.findOne({ email: userEmail });
+
+            // If the user isn't found set invalid credentials
+            if (!user) {
+              success = false;
+              msg = 'Invalid Credentials';
+            }
+            
+            // Else (if the user exists), continue
+            else {
+                // Next compare plain text & encrypted password
+                // user.password comes from User.findOne
+                const isMatch = await bcryptjs.compare(
+                    userPassword,
+                    user.password
+                );
+
+                // If the passwords do not match
+                if (!isMatch) {
+                    success = false;
+                    msg = 'Invalid Credentials';
+                }
+
+                // Else, continue onward (if the passwords do match)
+                else {
+                    /* user.findOne() returned a promise allowing us to access the
+                        record id user user.id. Mongoose abstracts ._id to .id */
+
+                    // Construct JWT payload
+                    const payload = {
+                    user: {
+                        id: user.id,
+                    },
+                    };
+
+                    // Load getToken function
+                    function getToken() {
+                    return new Promise((resolve, reject) => {
+                        jwt.sign(
+                        payload,
+                        config.get('jwtSecret'),
+                        { expiresIn: config.get('jwtDuration') },
+                        (err, token) => {
+                            if (err) reject(err);
+                            else resolve(token);
+                        }
+                        );
+                    });
+                    }
+
+                    // Set jwtToken
+                    jwtToken = await getToken();
+
+                    // Set rest of response
+                    success = true;
+                    msg = 'JWT Token';
+
+                    // Emit userLoggedIn event
+                    authServiceEvents.emit('userLoggedIn');
+                }
+            };
+
+            
+
+            // Build response
+            response = {
+                success: success,
+                msg: msg,
+                jwtToken: jwtToken
+            };
+
+            // Return response object
+            return response;
+
+        } catch (err) {
+            console.error(err.message);
+            return {
+                success: false,
+                msg: 'Server Error'
+            };
+        };
+    };
+};
